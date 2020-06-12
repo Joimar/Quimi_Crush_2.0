a4d4efb4c42a990984c7d8fad6469c4f86e4f099
joi <joimarbgf@gmail.com>
2020-06-12 19:57:54 -0300

    Ponto inicial de projeto padrão

---
+++ android/src/com/quimicrush/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.quimicrush.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.quimicrush.game.QuimiCrush;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new QuimiCrush(), config);
+	}
+}
---
+++ desktop/src/com/quimicrush/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quimicrush.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quimicrush.game.QuimiCrush;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QuimiCrush(), config);
+	}
+}
